571133833404f0420d7fe7bfbf5266a37c35764f
Doug Krahmer <douglas.krahmer@viasat.com>
2022-06-14 13:42:24 -0700

    Add IsStartMinimized argument Add Options class

---
+++ ConsoleSystemTray/Options.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleSystemTray
+{
+  internal class Options
+  {
+    public Options()
+    {
+    }
+
+    public string Path { get; set; }
+    public string Arguments { get; set; }
+    public string BaseDirectory { get; set; }
+    public string Icon { get; set; }
+    public string Tip { get; set; }
+    public bool IsPreventSleep { get; set; }
+    public bool IsStartMinimized { get; set; }
+  }
+}
